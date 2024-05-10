57c790e127f678d026fc1d5746c39be83bdc3a48
Lu'luatul Maknunah <125227561+LuluatulMaknunah21-048@users.noreply.github.com>
2024-05-10 10:27:00 +0700

    Update finpro.py

--- finpro.py
+++ finpro.py
@@ -19,3 +19,42 @@ compositionG = st.number_input('compositionG')
 compositionH = st.number_input('compositionH')
 compositionI = st.number_input('compositionI')
 compositionJ = st.number_input('compositionJ')
+catb=['Silahkan Pilih','0','1']
+catbp=st.selectbox('PILIH Category B',catb)
+        if catb =='0':
+            df['categoryB_catB_0']=1
+        if catb =='1':
+            df['categoryB_catB_1']=1
+          
+catd=['Silahkan Pilih','0','1','2']
+catdp=st.selectbox('PILIH Category D',catd)
+        if catd =='0':
+            df['categoryD_catD_0']=1
+        if catd =='1':
+            df['categoryD_catD_1']=1
+        if catd =='2':
+            df['categoryD_catD_1']=1
+            
+catf=['Silahkan Pilih','0','1','2']
+catfp=st.selectbox('PILIH Category F',catf)
+        if catf =='0':
+            df['categoryD_catF_0']=1
+        if catf =='1':
+            df['categoryF_catF_1']=1
+        if catf =='2':
+            df['categoryF_catF_1']=1
+
+unit=['Silahkan Pilih','0','1','3']
+unitp=st.selectbox('PILIH UNIT',unit)
+        if unit =='0':
+            df['unit_unit_0']=1
+        if unit =='1':
+            df['unit_unit_1']=1
+        if unit =='2':
+            df['unit_unit_2']=1
+        if unit =='3':
+            df['unit_unit_3']=1
+        if unit =='4':
+            df['unit_unit_4']=1
+        if unit =='5':
+            df['unit_unit_5']=1
